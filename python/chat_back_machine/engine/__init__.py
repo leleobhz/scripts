6c88fa719673550931abfc2daed706d8177d1fdc
leleobhz <leleobhz@tarsila>
2009-12-26 23:52:45 -0200

    * Converted engine code to database view

--- python/chat_back_machine/engine/__init__.py
+++ python/chat_back_machine/engine/__init__.py
@@ -25,10 +25,14 @@ def walktree (top = ".", depthfirst = True):
 
 
 class kopeteLog():
-    masslog = []
     def __init__(self, directory=os.path.join(os.path.expanduser("~"), 
                         ".kde/share/apps/kopete/logs")):
-        self.requestLogList(directory)
+
+        self.messages = Base('kopete.db')  # Database stuff - Initializing...
+        self.messages.create('protocol', 'date', 'time', 'msgfrom', 'msgto',  'sender',  'inbound',  'nick',  'message',  mode='override')
+        
+        for file in self.searchLogs(directory):
+            self.feedDatabase(file)
         
     def searchLogs(self,  dir):
         logfiles = []
@@ -38,47 +42,47 @@ class kopeteLog():
                     logfiles.append(os.path.join(basepath, child))
         return logfiles
         
-    def requestLogList(self,  directory):
-        for file in self.searchLogs(directory):
-            if file.find('WlmProtocol'):
-                protocol = 'wlm'
-            elif file.find('ICQProtocol'):
-                protocol = 'icq'
-            elif file.find('JabberProtocol'):
-                protocol = 'jabber'
-            else:
-                protocol = ''
-            print "File: %s" % file
-            xmllog = parse(file)
-            for head in xmllog.getiterator('head'):
-                for date in head.getiterator('date'):
-                    month=date.attrib['month']
-                    year=date.attrib['year']
-                for contact in head.getiterator('contact'):
-                    if contact.attrib.has_key('type'):
-                        if contact.attrib['type'] == 'myself':
-                            contactfrom = contact.attrib['contactId']
-                    else:
-                        contactto = contact.attrib['contactId']
-            for msg in xmllog.getiterator('msg'):
-                nick = msg.attrib['nick']
-                time = msg.attrib['time']
-                inbound = msg.attrib['in']
-                message = msg.text
-                sender = msg.attrib['from']
-                date = datetime.strptime("%s;%s;%s" % 
-                                         (year,  month,  msg.attrib['time']) , 
-                                         "%Y;%m;%d %H:%M:%S")
-                self.masslog.append ( 
-                                     {'protocol' : protocol,  
-                                     'date' : date.strftime("%Y%m%d"), 
-                                     'time' : date.strftime("%H:%M:%S"),  
-                                     'from' : contactfrom,  'to' : contactto,  
-                                     'sender' : sender,  'inbound' : inbound, 
-                                     'nick' : nick,  'message' : message})
-
+    def feedDatabase(self,  file):
+        if file.find('WlmProtocol'):
+            protocol = 'wlm'
+        elif file.find('ICQProtocol'):
+            protocol = 'icq'
+        elif file.find('JabberProtocol'):
+            protocol = 'jabber'
+        else:
+            protocol = ''
+        xmllog = parse(file)
+        for head in xmllog.getiterator('head'):
+            for date in head.getiterator('date'):
+                month=date.attrib['month']
+                year=date.attrib['year']
+            for contact in head.getiterator('contact'):
+                if contact.attrib.has_key('type'):
+                    if contact.attrib['type'] == 'myself':
+                        contactfrom = contact.attrib['contactId']
+                else:
+                    contactto = contact.attrib['contactId']
+        for msg in xmllog.getiterator('msg'):
+            nick = msg.attrib['nick']
+            time = msg.attrib['time']
+            inbound = msg.attrib['in']
+            message = msg.text
+            sender = msg.attrib['from']
+            date = datetime.strptime("%s;%s;%s" % 
+                                     (year,  month,  msg.attrib['time']) , 
+                                     "%Y;%m;%d %H:%M:%S")
+            self.messages.insert(
+                                 protocol=protocol, 
+                                 date=date.strftime("%Y%m%d"), 
+                                 time=date.strftime("%H:%M:%S"),  
+                                 msgfrom=contactfrom,  msgto=contactto,
+                                 sender=sender,  inbound=inbound,  nick=nick,
+                                 message=message)
+    
+    def locate(self):
+        return self.messages(date='20091217')
+    
 if __name__ == "__main__":
     teste = kopeteLog()
-    print "Tamanho do log: %s mensagens" % len(teste.masslog)
-    print teste.masslog[16691]
+    print "%s\n\n" % teste.locate()[0]
     sys.exit(1)
